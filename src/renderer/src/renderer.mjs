db0219d6221f7c0709a9362bb1c7e171a02e3a64
TylerN-Xav <thanhtylernguyen203@gmail.com>
2025-08-01 22:12:49 -0400

    Fixing formatting in renderer code as well as fixing error in the preload code.

--- src/renderer/src/renderer.mjs
+++ src/renderer/src/renderer.mjs
@@ -1,48 +1,45 @@
-import got from "got";
-import clipboard from "clipboardy";
+import got from 'got'
+import clipboard from 'clipboardy'
 
 async function getUserData(user, key) {
   const url =
-      "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" +
-      user +
-      "&api_key=" +
-      key +
-      "&format=json&limit=1";
-  const result = await got(url);
-  return result.body;
+    'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' +
+    user +
+    '&api_key=' +
+    key +
+    '&format=json&limit=1'
+  const result = await got(url)
+  return result.body
 }
 
 function sayHello() {
-  console.log("Hello!")
+  console.log('Hello!')
 }
 
 async function copyUserSong() {
   while (true) {
-      
-      var origSong = "";
-      const apiKey = "d8b65e4db3fc7db8f5be9fada062ef1d";
-      let username = document.getElementById("name").data;
+    var origSong = ''
+    const apiKey = 'd8b65e4db3fc7db8f5be9fada062ef1d'
+    let username = document.getElementById('name').data
 
-      let result = await getUserData(username, apiKey);
-      let data = JSON.parse(result);
+    let result = await getUserData(username, apiKey)
+    let data = JSON.parse(result)
 
-      let playingPrefix = "Last played: ";
-      if (Object.hasOwn(data.recenttracks.track[0], "@attr")) {
-          playingPrefix = "Now playing: ";
-      }
+    let playingPrefix = 'Last played: '
+    if (Object.hasOwn(data.recenttracks.track[0], '@attr')) {
+      playingPrefix = 'Now playing: '
+    }
 
-      let currSong = data.recenttracks.track[0].name;
-      let currArtist = data.recenttracks.track[0].artist["#text"];
+    let currSong = data.recenttracks.track[0].name
+    let currArtist = data.recenttracks.track[0].artist['#text']
 
-      let fullText = playingPrefix + currSong + " by " + currArtist;
-      console.log(fullText);
+    let fullText = playingPrefix + currSong + ' by ' + currArtist
+    console.log(fullText)
 
-      if (origSong != currSong) {
-          origSong = currSong;
-          console.log("Writing!");
-          clipboard.writeSync(fullText);
-      }
-
+    if (origSong != currSong) {
+      origSong = currSong
+      console.log('Writing!')
+      clipboard.writeSync(fullText)
+    }
   }
-
 }
